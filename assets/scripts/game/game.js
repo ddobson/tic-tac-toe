efc3435a0e500149c04cb3e1b15cd343fe8cb7f6
ddobson <ddobson445@gmail.com>
2017-01-24 18:19:50 -0500

    Complete win checking functionality Also includes update to package.json

--- assets/scripts/game/game.js
+++ assets/scripts/game/game.js
@@ -1,29 +1,46 @@
 'use strict';
 
-// [
-// 0,1,2,
-// 3,4,5,
-// 6,7,8
-// ]
-
-const winningStates = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+const winningStates = [
+                        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                        [0, 4, 8], [2, 4, 6],
+                      ];
 
 const Game = function (board) {
   this.board = board;
-  this.turn = 'x';
-
-  this.winner = function () {
-    return board;
-  };
+  this.playerTurn = 'x';
 
   this.getIndexes = function () {
     let indexes = [];
     for (let i = 0; i < this.board.length; i++) {
-      if (this.board[i] === this.turn) {
+      if (this.board[i] === this.playerTurn) {
         indexes.push(i);
       }
     }
+
     return indexes;
+  };
+
+  this.compareArrays = function (playerSet, winSet) {
+    return winSet.every((e) => {
+      return (playerSet.indexOf(e) >= 0);
+    });
+  };
+
+  this.checkWinner = function () {
+    const indexes = this.getIndexes();
+    let winner = null;
+
+    for (let i = 0; i < winningStates.length; i++) {
+      const isWinner = this.compareArrays(indexes, winningStates[i]);
+
+      if (isWinner) {
+        winner = this.playerTurn;
+        break;
+      }
+    }
+
+    return winner;
   };
 };
 
