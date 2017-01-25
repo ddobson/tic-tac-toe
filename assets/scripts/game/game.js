33867eb799fdb46e317d50b7c90a2d88bf2691f9
ddobson <ddobson445@gmail.com>
2017-01-24 21:34:44 -0500

    Reset function & variable moves Create a function to reset the game & move global vars into game object

--- assets/scripts/game/game.js
+++ assets/scripts/game/game.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const winningStates = [
-                        [0, 1, 2], [3, 4, 5], [6, 7, 8],  //rows
-                        [0, 3, 6], [1, 4, 7], [2, 5, 8],  //columns
-                        [0, 4, 8], [2, 4, 6],             //diagonals
-                      ];
-
 const Game = function (board) {
   this.board = board;
   this.currentPlayer = 'x';
+  this.newBoard = [null, null, null, null, null, null, null, null, null];
+  this.winningStates = [
+                          [0, 1, 2], [3, 4, 5], [6, 7, 8],  //rows
+                          [0, 3, 6], [1, 4, 7], [2, 5, 8],  //columns
+                          [0, 4, 8], [2, 4, 6],             //diagonals
+                        ];
 
   this.getIndexes = function () {
     let indexes = [];
@@ -32,8 +32,8 @@ const Game = function (board) {
     const indexes = this.getIndexes();
     let winner = null;
 
-    for (let i = 0; i < winningStates.length; i++) {
-      const isWinner = this.compareArrays(indexes, winningStates[i]);
+    for (let i = 0; i < this.winningStates.length; i++) {
+      const isWinner = this.compareArrays(indexes, this.winningStates[i]);
 
       if (isWinner) {
         winner = this.currentPlayer;
@@ -54,6 +54,12 @@ const Game = function (board) {
     return this.currentPlayer;
   };
 
+  this.reset = function () {
+    this.currentPlayer = 'x';
+    this.board = this.newBoard;
+    return this.board;
+  };
+
 };
 
 module.exports = {
@@ -62,4 +68,3 @@ module.exports = {
 
 // const tic = require('./assets/scripts/game/game.js');
 // const game = new tic.Game(['x','x',null,'o','o',null,null,null,null]);
-//
